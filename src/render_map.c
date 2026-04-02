3a023f86602df36a9ee8c95cc2da955b9b7cd952
Ador Trombella <atrombel@c2r1s6.42lausanne.ch>
2026-04-02 14:19:35 +0200

    run sur pc ecoleok

--- src/render_map.c
+++ src/render_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render_map.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atrombel <atrombel@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*   By: atrombel <atrombel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/29 16:51:23 by atrombel          #+#    #+#             */
-/*   Updated: 2026/03/29 16:51:26 by atrombel         ###   ########.fr       */
+/*   Updated: 2026/04/02 14:17:03 by atrombel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,22 @@ void	ft_render_x(t_struct *game, int x, int y)
 	while (x < game->map_length_x)
 	{
 		mlx_put_image_to_window(game->init,
-			game->window, game->floor, x * TILE_SIZE, y * TILE_SIZE);
+			game->window, game->floor, x * TILE, y * TILE);
 		if (game->map[y][x] == '1')
 			mlx_put_image_to_window(game->init,
-				game->window, game->wall, x * TILE_SIZE, y * TILE_SIZE);
+				game->window, game->wall, x * TILE, y * TILE);
 		else if (game->map[y][x] == 'P')
 			mlx_put_image_to_window(game->init,
-				game->window, game->player, x * TILE_SIZE, y * TILE_SIZE);
+				game->window, game->player, x * TILE, y * TILE);
 		else if (game->map[y][x] == 'C')
 			mlx_put_image_to_window(game->init,
-				game->window, game->item, x * TILE_SIZE, y * TILE_SIZE);
+				game->window, game->item, x * TILE, y * TILE);
 		else if (game->map[y][x] == 'E')
 			mlx_put_image_to_window(game->init,
-				game->window, game->exit_closed, x * TILE_SIZE, y * TILE_SIZE);
+				game->window, game->exit_closed, x * TILE, y * TILE);
 		else if (game->map[y][x] == 'F')
 			mlx_put_image_to_window(game->init,
-				game->window, game->exit_open, x * TILE_SIZE, y * TILE_SIZE);
+				game->window, game->exit_open, x * TILE, y * TILE);
 		x++;
 	}
 }
